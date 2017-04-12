184f4db2a634b878baeb3b57f3969cbe4066338c
Fernando Nunes <domusonline@gmail.com>
2017-04-12 09:45:13 +0100

    Test

--- healthcheck/attached_indexes.sql
+++ healthcheck/attached_indexes.sql
@@ -1,9 +1,8 @@
 -----------------------------------------------------------------------------------------------------------------
---
 -- Copyright (c) 2010-2017 Fernando Nunes - domusonline@gmail.com
 -- $Author: Fernando Nunes - domusonline@gmail.com $
--- $Revision: 2.0.3 $
--- $Date 2016-04-12 16:11:06$
+-- $Revision: 1.0.1 $
+-- $Date 2017-04-12 09:45:13$
 -- Disclaimer: This software is provided AS IS, without any kind of guarantee. Use at your own risk.
 --             Although the author is/was an IBM employee, this software was created outside his job engagements.
 --             As such, all credits are due to the author.
